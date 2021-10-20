486d3f5aac3c95085fb406b11f6be7f2bae2efa3
Huy Nguyen <khangwhat116@gmail.com>
2021-10-20 16:45:08 +0700

    fix productrouter

--- src/Router/ProductRouter.js
+++ src/Router/ProductRouter.js
@@ -5,20 +5,23 @@ import InsertProductPage from "./../pages/QLProduct/InsertProductPage";
 import DetailsProduct from "./../pages/QLProduct/DetailsProduct";
 import UpdateProduct from "./../pages/QLProduct/UpdateProduct";
 import ProtectedRoute from "../pages/auth/ProtectedRoute";
-<<<<<<< HEAD
-import Toolbar from "@mui/material/Toolbar";
-
-=======
 import UserPage from "../pages/QLUser/UserPage";
 import ErrorErrorURLMain from "../pages/PageNotFound/ErrorURLMain";
->>>>>>> 04a74c06c76258f09c4610b14bf959513ac0712e
 const ProductRouter = () => {
   return (
     <Router>
       <Switch>
         {/* Sai đường dẫn sẽ vào trang NotFound này */}
-        <Route path="/MainDrawer/qlsanpham/sanpham" exact component={ProductPage} />
-        <Route path="/MainDrawer/qlsanpham/sanpham/:id" exact component={DetailsProduct} />
+        <Route
+          path="/MainDrawer/qlsanpham/sanpham"
+          exact
+          component={ProductPage}
+        />
+        <Route
+          path="/MainDrawer/qlsanpham/sanpham/:id"
+          exact
+          component={DetailsProduct}
+        />
         <Route
           path="/MainDrawer/qlsanpham/addsanpham"
           exact
@@ -29,11 +32,7 @@ const ProductRouter = () => {
           exact
           component={UpdateProduct}
         />
-        <Route
-          path="/MainDrawer/ggg"
-          exact
-          component={UserPage}
-        />
+        <Route path="/MainDrawer/ggg" exact component={UserPage} />
         <Route component={ErrorErrorURLMain} />
       </Switch>
     </Router>
